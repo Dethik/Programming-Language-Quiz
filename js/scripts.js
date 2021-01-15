60f4ce830706bb5bb5ba1911980c9983be6da129
thorgrim88@gmail.com <thorgrim88@gmail.com>
2021-01-15 09:58:45 -0800

    add basic css elements and fix js syntax

--- js/scripts.js
+++ js/scripts.js
@@ -6,17 +6,17 @@ $(document).ready(function() {
     const gaming = $("input#gaming").val();
     const mobile = $("input#mobile").val();
     const web = $("input#web").val();
-    if (gaming === yes && mobile === yes) {
+    if (gaming === "yes" && mobile === "yes") {
       $('#java').show();
       $('#cplusplus').hide();
       $('#ruby').hide();
     } 
-    else if (gaming === yes) {
+    else if (gaming === "yes") {
       $('#cplusplus').show();
       $('#java').hide();
       $('#ruby').hide();
     }
-    else if (web === yes || color === yes) {
+    else if (web === "yes" || color === "yes") {
       $('#ruby').show();
       $('#cplusplus').hide();
       $('#java').hide();
