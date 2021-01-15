8b00735e45ee236267b90d3ecdd7b00c7b5cecc2
thorgrim88@gmail.com <thorgrim88@gmail.com>
2021-01-15 09:23:07 -0800

    add branching logic to js and fix syntax in html

--- js/scripts.js
+++ js/scripts.js
@@ -8,6 +8,18 @@ $(document).ready(function() {
     const web = $("input#web").val();
     if (gaming === yes && mobile === yes) {
       $('#java').show();
+      $('#cplusplus').hide();
+      $('#ruby').hide();
+    } 
+    else if (gaming === yes) {
+      $('#cplusplus').show();
+      $('#java').hide();
+      $('#ruby').hide();
+    }
+    else if (web === yes || color === yes) {
+      $('#ruby').show();
+      $('#cplusplus').hide();
+      $('#java').hide();
     }
   });
 });
